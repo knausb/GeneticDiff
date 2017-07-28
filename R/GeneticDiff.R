02a409251803b493775f6a8fbade86b9c9f01f3e
Brian J. Knaus <briank.lists@gmail.com>
2017-07-28 11:22:59 -0700

    added Rcpp

---
+++ R/GeneticDiff.R
@@ -0,0 +1,16 @@
+#'
+#' @title GeneticDiff
+#' @name GeneticDiff
+#' @rdname GeneticDiff
+#' 
+#' @description Genetic differentiation
+#' 
+#' @details 
+#' GeneticDiff provides tools to explore and measure genetic differentiation.
+#' Functionality is included to simulate data, measure within population diversity and among population differentiation.
+#' 
+#'
+#'
+#' @useDynLib GeneticDiff
+#' @importFrom Rcpp sourceCpp
+NULL
