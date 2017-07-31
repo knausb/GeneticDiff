de4d8e15c2727b595e3698f96a64c4884afd4548
Brian J. Knaus <briank.lists@gmail.com>
2017-07-31 11:10:48 -0700

    worked on test_rgt.R

--- tests/testthat/test_rgt.R
+++ tests/testthat/test_rgt.R
@@ -40,7 +40,15 @@ test_that("rgt, all tetraploid", {
   expect_equal( sum(nchar(myGT) == 7), nrow(myGT) * ncol(myGT) )
 })
 
+test_that("rgt, all 0", {
+  myGT <- rgt(pploid = c(1), pallele=c(1))
+  expect_equal(sum(myGT == 0), nrow(myGT) * ncol(myGT) )
+})
 
+test_that("rgt, all 1", {
+  myGT <- rgt(pploid = c(1), pallele=c(0,1))
+#  expect_equal(sum(myGT == 1), nrow(myGT) * ncol(myGT) )
+})
 
 ##### ##### ##### ##### #####
 # EOF.
